aeb5aaedb5d80b4b96d18f65b0b5600884481ccf
Joao Marques <joao.marques.code@gmail.com>
2019-01-27 17:05:01 +0000

    My Portfolio

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer></v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
